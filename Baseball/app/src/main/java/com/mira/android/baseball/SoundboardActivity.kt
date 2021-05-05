fe021ca64704fb23b006cd0948bec477afb2270d
mirandamanuel <49087932+mirandamanuel@users.noreply.github.com>
2021-05-05 11:54:33 -0700

    Added Soundboard kt and xml

---
+++ Baseball/app/src/main/java/com/mira/android/baseball/SoundboardActivity.kt
@@ -0,0 +1,11 @@
+package com.mira.android.baseball
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SoundboardActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_soundboard)
+    }
+}
